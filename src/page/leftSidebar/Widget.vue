ab71a7dfa3c9bee2ac8b6fb9680c4a98a1f4f7a6
Голосов Денис Леонидович <golosov@proitr.ru>
2019-11-25 19:03:34 +0500

    widget tab display changed

--- src/page/leftSidebar/Widget.vue
+++ src/page/leftSidebar/Widget.vue
@@ -1,22 +1,29 @@
 <template>
-    <el-row :gutter="8">
+    <el-row :gutter="8" class="collapse-widget">
         <el-col v-if="wSelect">
-            <div @click="highlightWidget(wSelect)">
-                <el-divider content-position="right">
-                    {{ wSelect.id }}
-                    <i class="el-icon-star-off"></i>
+            <div class="widget"
+                 @click="highlightWidget(wSelect)">
+
+                <el-divider content-position="center">
+                    <i class="el-icon-star-off copy"
+                       @click="logWidget(wSelect.id)"></i>
                 </el-divider>
                 <div v-html="wSelect.text"></div>
             </div>
 
             <div v-for="wChild in wChild_"
                  :key="wChild.w.id"
+
+                 class="widget"
                  @click="highlightWidget(wChild.w)">
 
-                <el-divider content-position="right">
-                    {{ wChild.w.id }}
-                    <i :class="getLineDirectionIcon(wChild.lineDirection)"></i>
+                <el-divider content-position="center">
                     <i class="el-icon-star-off"></i>
+                    <i :class="getLineDirectionIcon(wChild.lineDirection)"
+                       class="copy"
+                       @click="logWidget(wChild.wLine.id)"></i>
+                    <i class="el-icon-full-screen copy"
+                       @click="logWidget(wChild.w.id)"></i>
                 </el-divider>
                 <div v-html="wChild.w.text"></div>
             </div>
@@ -72,25 +79,32 @@
                 )
 
                 each( wLine_, async wLine => {
-                    let lineDirection = ( wLine.startWidgetId === this.wSelect.id
-                        ? constant.lineDirection.OUT
-                        : constant.lineDirection.IN )
-                    let wChildId      = ( lineDirection === constant.lineDirection.OUT
+                    let lineDirection = (
+                        wLine.startWidgetId === this.wSelect.id
+                            ? constant.lineDirection.OUT
+                            : constant.lineDirection.IN )
+                    let wChildId      = (
+                        lineDirection === constant.lineDirection.OUT
                             ? wLine.endWidgetId
                             : wLine.startWidgetId
                     )
-                    let wChild        = await this.getWidgetById( wChildId )
-                    this.wChild_.push( {
-                        w: wChild,
-                        lineDirection,
-                    } )
+
+                    const wTagId_ = await this.getWidgetTagId_()
+                    if( ! wTagId_.includes( wChildId ) ){
+                        let wChild = await this.getWidgetById( wChildId )
+                        this.wChild_.push( {
+                            w: wChild,
+                            wLine,
+                            lineDirection,
+                        } )
+                    }
                 } )
             },
 
-            getLineDirectionIcon(lineDirection){
-                return (lineDirection === constant.lineDirection.OUT
-                    ? 'el-icon-back'
-                    : 'el-icon-right')
+            getLineDirectionIcon( lineDirection ){
+                return ( lineDirection === constant.lineDirection.OUT
+                    ? 'el-icon-right'
+                    : 'el-icon-back' )
             },
 
             async highlightWidget( w ){
@@ -123,9 +137,47 @@
 
                 miro.board.widgets.__blinkWidget( w_ )
             },
+
+            async logWidget( wId ){
+                console.log( await this.getWidgetById( wId ) )
+            }
         }
     }
 </script>
 
-<style>
+<style lang="scss">
+    .collapse-widget {
+        .widget {
+            cursor        : pointer;
+            border-radius : 4px;
+            transition    : .2s linear;
+            padding       : 1em;
+            border        : 1px solid white;
+
+            &:hover {
+                border : 1px solid #409EFF;
+                /*box-shadow: 0 0 0 2pt #409EFF inset, 0 0 0 4pt white inset;*/
+            }
+
+            .copy {
+                cursor : copy;
+            }
+
+            p {
+                &:first-child {
+                    margin  : 1em;
+                    display : block;
+                }
+
+                margin  : 1px;
+                display : inline;
+            }
+
+            .el-divider {
+                i {
+                    margin : 0 4px;
+                }
+            }
+        }
+    }
 </style>
